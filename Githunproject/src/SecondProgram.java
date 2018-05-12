7015f07a91bc55577afc6910af93d0f6caa03fc4
Sasitharan <Sasitharan@Sasitharan-PC>
2018-05-12 10:57:00 +0530

    New java file added

---
+++ Githunproject/src/SecondProgram.java
@@ -0,0 +1,15 @@
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	System.out.println("Third program");
+	System.out.println("Third uasudbas program");
+	System.out.println("Third jkhkjabjksd as program");
+	
+	
+	}
+
+}
